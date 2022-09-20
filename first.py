cf1382478f20ebd5282f161fe9e25d51b2a60383
Deeksha-adepu <adepudeeksha441@gmail.com>
2022-09-20 16:34:25 +0530

    hello

---
+++ first.py
@@ -0,0 +1,18 @@
+#first program
+a=10
+print(a,type(a))
+b=3.78
+print(b,type(b))
+c=1+8j
+print(c,type(c))
+d=True
+print(d,type(d))
+s="Python"
+print(s,type(s))
+
+#collections
+lst=[10,20,30,40]
+print(lst,type(lst))
+tpl=(10,12.3,"python")
+print(tpl,type(tpl))
+      
